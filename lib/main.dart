934cb48cea5992632b940d7d46aa759d371d5228
Kumar Saurabh <55243385+KS9004@users.noreply.github.com>
2020-06-06 21:10:23 +0530

    added login and sign-up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:ais_app/intro_screen.dart';
+import 'package:ais_app/screens/login_screen.dart';
+import 'package:ais_app/screens/registration_screen.dart';
+import 'package:ais_app/screens/welcome_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,67 +20,14 @@ class MyApp extends StatelessWidget {
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Welcome_Screen(),
+      initialRoute: Intro_Screen.id,
+      routes: {
+        Intro_Screen.id:(context)=>Intro_Screen(),
+        WelcomeScreen.id:(context)=>WelcomeScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        RegistrationScreen.id:(context)=>RegistrationScreen(),
+      },
     );
   }
 }
 
-class Welcome_Screen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child:Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomRight,
-              end:Alignment.topLeft,
-              colors: [Color.fromRGBO(28, 31, 52, 1),Color.fromRGBO(6, 139, 255, 1)],
-            ),
-          ),
-          child: Column(
-
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top:50.0,),
-                child: Text("Welcome!",style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Mania',
-                ),),
-              ),
-              Text("To Automated Irrigation System",style: TextStyle(
-                color: Colors.white54,
-              ),
-              ),
-              SizedBox(
-                height: 80.0,
-              ),
-              Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(25.0),
-                color: Colors.pink,
-
-                child: MaterialButton(onPressed: null,
-                  minWidth: MediaQuery.of(context).size.width/2.5,
-                  padding: EdgeInsets.all(5.0),
-
-                  child: Text("Get Started",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40.0,
-                    fontFamily: 'Mania'
-                  ),),
-                ),
-              )
-            ],
-          ),
-      ),
-      ),
-    );
-  }
-}
